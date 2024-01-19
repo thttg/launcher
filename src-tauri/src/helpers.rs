0c8df4256575f3d8901595634a0497295d248c34
TTG <ttgcanada@outlook.com>
2024-01-19 18:21:34 -0500

    621

--- src-tauri/src/helpers.rs
+++ src-tauri/src/helpers.rs
@@ -24,37 +24,52 @@ pub fn decode_buffer(buf: Vec<u8>) -> (String, String) {
         .unwrap_or_else(|| "not_found".to_string());
 
     // Collect encoding results for debug
-    let mut messages: Vec<String> = Vec::new();
-    messages.push(format!("Input: {}", String::from_utf8_lossy(&buf)));
-    messages.push(format!("\tchardetng: {}", chardetng_encoding));
-    messages.push(format!("\tchardet: {}", chardet_encoding));
-    messages.push(format!(
-        "\tcharset_normalizer: {}",
-        charset_normalizer_encoding
-    ));
+    // let mut messages: Vec<String> = Vec::new();
+    // messages.push(format!("Input: {}", String::from_utf8_lossy(&buf)));
+    // messages.push(format!("\tchardetng: {}", chardetng_encoding));
+    // messages.push(format!("\tchardet: {}", chardet_encoding));
+    // messages.push(format!(
+    //     "\tcharset_normalizer: {}",
+    //     charset_normalizer_encoding
+    // ));
 
     // Determine the most likely actual encoding
-    let actual_encoding = if chardet_encoding == "ascii" && charset_normalizer_encoding == "ascii" {
+    let actual_encoding = if chardet_encoding.to_lowercase() == "ascii"
+        && charset_normalizer_encoding.to_lowercase() == "ascii"
+    {
         // Default to UTF-8 if both chardet and charset normalizer detect ASCII
         Encoding::for_label("UTF_8".as_bytes()).unwrap_or(UTF_8)
-    } else if (chardetng_encoding == "GBK" && charset_normalizer_encoding == "windows-874")
-        || (chardetng_encoding == "GBK" && charset_normalizer_encoding == "ibm866")
-        || charset_normalizer_encoding == "macintosh"
+    } else if chardet_encoding.to_lowercase() == "koi8-r"
+        && charset_normalizer_encoding.to_lowercase() == "koi8-r"
     {
-        // Use windows-1251 for GBK and windows-874 or GBK and IBM866 combination, or when charset normalizer detects macintosh
+        // Use windows-1251 if both chardet and charset normalizer detect KOI8-R
         Encoding::for_label("windows-1251".as_bytes()).unwrap_or(UTF_8)
-    } else if (chardetng_encoding == "windows-1252" && chardet_encoding == "windows-1251")
-        || (chardet_encoding == "ISO-8859-1"
-            && (charset_normalizer_encoding == "ibm866"
-                || charset_normalizer_encoding == "iso-8859-2"
-                || charset_normalizer_encoding == "windows-874"
-                || charset_normalizer_encoding == "iso-8859-1"))
-        || (chardetng_encoding == "GBK" && chardet_encoding == "ISO-8859-1")
+    } else if (chardetng_encoding.to_lowercase() == "gbk"
+        && (chardet_encoding.to_lowercase() == "windows-1255"
+            || charset_normalizer_encoding.to_lowercase() == "ibm866"))
+        || chardet_encoding.to_lowercase() == "x-mac-cyrillic"
+        || charset_normalizer_encoding.to_lowercase() == "macintosh"
+    {
+        // Use windows-1251 for various combinations
+        Encoding::for_label("windows-1251".as_bytes()).unwrap_or(UTF_8)
+    } else if (chardetng_encoding.to_lowercase() == "windows-1252"
+        && chardet_encoding.to_lowercase() == "windows-1251")
+        || (chardet_encoding.to_lowercase() == "iso-8859-1"
+            && (charset_normalizer_encoding.to_lowercase() == "iso-8859-2"
+                || charset_normalizer_encoding.to_lowercase() == "windows-874"
+                || charset_normalizer_encoding.to_lowercase() == "iso-8859-1"
+                || charset_normalizer_encoding.to_lowercase() == "ibm866"))
+        || (chardetng_encoding.to_lowercase() == "gbk"
+            && chardet_encoding.to_lowercase() == "iso-8859-1")
+        || (chardetng_encoding.to_lowercase() == "shift_jis"
+            && chardet_encoding.to_lowercase() == "iso-8859-1")
     {
         // Use windows-1252 for various combinations
         Encoding::for_label("windows-1252".as_bytes()).unwrap_or(UTF_8)
-    } else if chardetng_encoding == "GBK" || chardet_encoding == "GB2312" {
-        // Use GB18030 for Chinese when chardetng detects GBK or chardet detects GB2312
+    } else if chardetng_encoding.to_lowercase() == "gbk"
+        || chardet_encoding.to_lowercase() == "gb2312"
+    {
+        // Use GB18030 when chardetng detects GBK or chardet detects GB2312
         Encoding::for_label("GB18030".as_bytes()).unwrap_or(UTF_8)
     } else {
         // Default to the encoding detected by chardetng
@@ -69,12 +84,12 @@ pub fn decode_buffer(buf: Vec<u8>) -> (String, String) {
     let buff_output = decoded.into_owned();
 
     // Print debug information
-    messages.push(format!("\tfinal: {}", actual_encoding.name().to_string()));
-    for message in messages {
-        print!("{}", message);
-        print!("\t");
-    }
-    println!();
+    // messages.push(format!("\tfinal: {}", actual_encoding.name().to_string()));
+    // for message in messages {
+    //     print!("{}", message);
+    //     print!("\t");
+    // }
+    // println!();
 
     // Return the decoded string and the encoding name
     (buff_output, actual_encoding.name().to_string())
